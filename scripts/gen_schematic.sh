87de1c58699f68fdb30a0c798c20e3872308ebdc
Yi Qingliang <niqingliang2003@gmail.com>
2018-12-29 17:34:54 +0800

    start test metapost context function
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

---
+++ scripts/gen_schematic.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+set -e
+
+SELFDIR="`dirname $0`"
+MAINDIR="`readlink -fe ${SELFDIR}/../`"
+
+FIGDIR="${MAINDIR}/fundamentals_of_electric_circuits/fig/"
+
+TMPDIR="`mktemp -p /tmp -d context.XXXX`"
+SRCFILE="$1"
+if [ ! -f ${SRCFILE} ]; then
+	echo "$0 {file name}$"
+	exit 1
+fi
+
+TMPFILE="${TMPDIR}/tmp.tex"
+
+echo "\starttext
+\switchtobodyfont[6pt]
+\startMPpage
+input ${FIGDIR}/schematic.mp;
+elesize_ratio := 3;
+eleconn_ratio := 6;
+labeloffset := 1;
+clearObjsForTwiceRun;
+%setX("fig" & str #1);
+input ${FIGDIR}/${SRCFILE}
+\stopMPpage
+\stoptext
+" > ${TMPFILE}
+
+context --purgeall ${TMPFILE}
+
+echo "rming ${TMPDIR}"
+rm -rf ${TMPDIR}
